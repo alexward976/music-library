ee47c4d3b2fadaffdbc2baf872060fa889ad9768
Alex Ward <alexward976@gmail.com>
2023-11-25 22:04:56 -0700

    changed github callback url to render

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const GitHubStrategy = require('passport-github2').Strategy;
 passport.use(new GitHubStrategy({
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: `http://127.0.0.1:${process.env.port || 8080}/auth/github/callback`
+        callbackURL: `https://music-library-72kl.onrender.com/auth/github/callback`
     },
     function(accessToken, refreshToken, profile, done) {
         process.nextTick(function() {
